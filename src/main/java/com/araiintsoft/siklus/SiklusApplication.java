2bf144a9f48130473d843a32e3740f5c34968c0a
araiihadi <araiihadi@gmail.com>
2019-05-29 00:39:00 +0700

    First Commit Siklus Twibbon

---
+++ src/main/java/com/araiintsoft/siklus/SiklusApplication.java
@@ -0,0 +1,13 @@
+package com.araiintsoft.siklus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiklusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiklusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/araiintsoft/siklus/SiklusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.araiintsoft.siklus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SiklusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
